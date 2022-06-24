c547b0134d4cc63655de4764a92ced171971baa9
Chang Liu <cliu86@bu.edu>
2022-06-24 16:41:32 +0800

    add course content

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,46 @@
-import fs from 'fs/promises'
-import path from 'path'
-const HomePage = props => {
-  const { products } = props
+import path from 'path';
+import fs from 'fs/promises';
+
+import Link from 'next/link';
+
+function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-    {
-      products.map(product=>(
-      <li key={product.id}>{product.title}</li>
-      )) 
-    }
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
 
-export async function getStaticProps(context){
-  //cwd - current working directory
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
-  const jsonData =  await fs.readFile(filePath)
-  const data = JSON.parse(jsonData)
-  if(!data){
-    return { redirect: {
-      destination: '/no-data'
-    }}
+export async function getStaticProps(context) {
+  console.log('(Re-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
   }
-  if(!data.products){
-    return { notFound: true}
+
+  if (data.products.length === 0) {
+    return { notFound: true };
   }
+
   return {
     props: {
-      products: data.products
+      products: data.products,
     },
-    revalidate: 10, 
-    
-  }
+    revalidate: 10,
+  };
 }
 
-export default HomePage
+export default HomePage;
